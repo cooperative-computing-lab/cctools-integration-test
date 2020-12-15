72b147abda0a101a292065db87f082ce387bbedb
Douglas Thain <dthain@nd.edu>
2020-12-15 08:34:13 -0500

    Bootstrap miniconda.

--- conda.sh
+++ conda.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh > conda-install.sh
-#bash conda-install.sh -p `pwd`/miniconda -b
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh > conda-install.sh
+bash conda-install.sh -p `pwd`/miniconda -b
 export PATH=`pwd`/miniconda/bin:$PATH
 conda install -y -c conda-forge ndcctools
 
