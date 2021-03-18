d1b317fcb846711a6001572053ff068860d594d2
Benjamin Tovar <btovar@nd.edu>
2021-03-18 08:17:44 -0400

    test spack from cctools-packaging-test label

--- spack.sh
+++ spack.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/spack/spack spack
+#git clone https://github.com/spack/spack spack
+git clone -b cctools-packaging-test --single-branch https://github.com/btovar/spack
+
 export SPACK_ROOT=`pwd`/spack
 . spack/share/spack/setup-env.sh
 spack install --test root cctools
