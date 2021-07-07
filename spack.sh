e70c4ecd4ac5217baf6f178e1b91d6ac19535a64
Douglas Thain <dthain@nd.edu>
2021-07-07 10:40:09 -0400

    Use standard Spack.

--- spack.sh
+++ spack.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#git clone https://github.com/spack/spack spack
-git clone -b cctools-packaging-test --single-branch https://github.com/btovar/spack
+git clone https://github.com/spack/spack spack
+#git clone -b cctools-packaging-test --single-branch https://github.com/btovar/spack
 
 export SPACK_ROOT=`pwd`/spack
 . spack/share/spack/setup-env.sh
