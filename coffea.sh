7f26ea30573aa31b93228af6b85546e7550838b0
Benjamin Tovar <btovar@nd.edu>
2021-10-26 10:16:36 -0400

    drop conda-pack from coffea.sh (comes from ndcctools)

--- coffea.sh
+++ coffea.sh
@@ -11,7 +11,7 @@ echo "*** Install Conda and Pip packages"
 conda create -y --name coffea-env
 conda activate coffea-env
 conda install -y python=3.8.3 six dill
-conda install -y -c conda-forge coffea ndcctools conda-pack xrootd uproot
+conda install -y -c conda-forge coffea ndcctools xrootd
 
 echo "*** Create the Conda-Pack tarball"
 conda-pack --name coffea-env --output coffea-env.tar.gz
