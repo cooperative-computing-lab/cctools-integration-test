0b66cc3f04b7ff80f4d764c456db0de6c18cf58d
Benjamin Tovar <btovar@nd.edu>
2021-10-26 10:12:32 -0400

    update python for parsl, drop conda-pack from instal list

--- parsl.sh
+++ parsl.sh
@@ -1,22 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 # Test for integration of Parsl and Work Queue starting from Conda install.
 
 # Fix for local environment at ND: unset PYTHONPATH to ignore existing python installs.
 export PYTHONPATH=
+export CONDA_ENV=./wq_parsl
+
+trap cleanup EXIT
+
+cleanup() {
+    rm -rf ${CONDA_ENV}
+}
+
 
 # Activate the Conda shell hooks without starting a new shell.
 CONDA_BASE=$(conda info --base)
 . $CONDA_BASE/etc/profile.d/conda.sh
 
-# Create local Conda environment
-conda create --name wq_parsl python=3.6 conda-pack
-conda activate wq_parsl
 
-# Install software into the new environment.
-conda install -c conda-forge parsl ndcctools
+# Create local conda environment:
+conda create --yes --prefix ${CONDA_ENV} -c conda-forge python=3.9 parsl ndcctools
 
 # Run Parsl application with WQ option.
 # Note that it internally uses the local provider to start workers.
-python parsl-test.py
-exit $?
+conda run --prefix ${CONDA_ENV} python parsl-test.py
+status=$?
+
+exit $status
