cb773da2b990c4b7ae5f611f428609df819cbbf1
Thanh Son Phung <tphung@nd.edu>
2021-11-05 12:41:56 -0400

    Adds support for a clean process death.

--- shadho-test.sh
+++ shadho-test.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 
+#unset PYTHONPATH to ignore existing python installs.
 export PYTHONPATH=
 
+#activate the conda shell hooks without starting a new shell
 CONDA_BASE=$(conda info --base)
 . $CONDA_BASE/etc/profile.d/conda.sh
 
-conda create --name shadho-test-v5 -y
-conda activate shadho-test-v5
+#create conda environment for shadho
+conda create --name shadho-test -y
+conda activate shadho-test
+
+#install ndcctools and shadho
 conda install -c conda-forge ndcctools -y
-conda install pip -y
+conda install pip -y	#shadho is installed through pip
 python -m pip install shadho
-python -m shadho.workers.workqueue -M sin_ex &
+
+#run work_queue_worker. This command is heuristic and based on shadho code.
+$CONDA_PREFIX/bin/work_queue_worker -M sin_ex-tphung --cores 1 --single-shot &
+
+#get worker pid to kill later
 export SHADHO_WORKER_PID=$!
-python shadho-test-files/wq_sin.py
+
+#run manager with simple application
+python wq_sin.py
+
+#remove resulting files from shadho application
 rm results.json results.json.bak.1 
 rm shadho_master.debug shadho_master.log
+
+#kill the worker, to be sure
 kill -2 $SHADHO_WORKER_PID
+
+#deactivate and remove the created environment
 conda deactivate
-conda remove --name shadho-test-v5 --all -y
+conda remove --name shadho-test --all -y
+
+#kill the shell that runs this script
 kill -2 $$
