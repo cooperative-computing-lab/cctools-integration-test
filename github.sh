a62cc3baa527d0a84d66d8d533798de5f53b0f23
Douglas Thain <dthain@nd.edu>
2023-07-31 13:42:14 -0400

    Add basic build tests.

--- github.sh
+++ github.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+set -xe
+
 PREFIX=`pwd`/cctools
 git clone https://github.com/cooperative-computing-lab/cctools github-cctools
 cd github-cctools
+echo =============================================
 ./configure --prefix $PREFIX
+echo =============================================
+make
+echo =============================================
+make test
+echo =============================================
 make install
+echo =============================================
 cd ..
-
 export PATH=${PREFIX}/bin:${PATH}
-
 exec ./smoke.sh
+echo =============================================
