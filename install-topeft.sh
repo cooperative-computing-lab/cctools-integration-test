7041443b575cfc6723b91e9e551f08518ec7974e
Douglas Thain <dthain@nd.edu>
2023-10-26 14:55:58 -0400

    Install topeft using micromamba instead.

--- install-topeft.sh
+++ install-topeft.sh
@@ -2,6 +2,11 @@
 
 . install-common.sh
 
+# Installing fresh from conda is too slow, use micromamba instead.
+curl -L micro.mamba.pm/install.sh | bash
+source ~/.bashrc
+micromamba activate
+
 CFG_DIR=$(realpath topcoffea-conf)
 DATA_FILE=${CFG_DIR}/ttHJet_UL17_R1B14_NAOD-00000_10194_NDSkim.root
 CFG=$(realpath ${CFG_DIR}/UL17_private_ttH_for_CI.json)
@@ -14,8 +19,8 @@ fi
 git clone https://github.com/TopEFT/topeft.git
 cd topeft
 unset PYTHONPATH 
-conda env create -q -f environment.yml
-conda activate coffea-env
+micromamba create -y -n coffea-env -f environment.yml
+micromamba activate coffea-env
 pip install -e .
 cd ..
 
@@ -24,8 +29,4 @@ cd topcoffea
 unset PYTHONPATH
 pip install -e .
 cd ..
-
-
-
-
 
