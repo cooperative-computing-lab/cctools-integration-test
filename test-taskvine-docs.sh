10803aba644d333ee23e913f5d9ec7886cbebc28
Thomas Hieber <thieber@cclws17.cse.nd.edu>
2023-12-02 17:45:44 -0500

    Commit to remote Branch for docs-scraping script

---
+++ test-taskvine-docs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#install & activate conda env
+#. ./install-conda-prod.sh
+
+#install bs4 for webscraping
+conda install -y beautifulsoup4 bs4
+
+#run scraping script
+python3 scrape.py
+
+#install ndcctools from conda
+. conda-inst.sh
+
+#run quickstart.py from conda
+. run-python.sh &
+
+vine_worker localhost 9123 --single-shot
