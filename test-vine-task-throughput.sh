38fa65efe71a494092815892b290e18db25f3d0d
Elliot Kim <ekim33@cclws17.cse.nd.edu>
2024-12-02 18:19:44 -0500

    debug python throughput

--- test-vine-task-throughput.sh
+++ test-vine-task-throughput.sh
@@ -7,5 +7,5 @@
 . ./install-github.sh
 
 # Run the test case.
-python3 test-vine-task-throughput.py
-python3 test-vine-task-serverless.py
+python test-vine-task-throughput.py
+python test-vine-task-serverless.py
