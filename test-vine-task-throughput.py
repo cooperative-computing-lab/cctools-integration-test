67632b40dba1d07375fc3370c4a3d37927b02fcf
Elliot Kim <ekim33@crcfe01.crc.nd.edu>
2024-12-02 22:11:44 -0500

    combines serverless to one file and adds different error messages

--- test-vine-task-throughput.py
+++ test-vine-task-throughput.py
@@ -4,9 +4,16 @@ import ndcctools.taskvine as vine
 import time
 import sys
 
+def func():
+    return 
+
 def main():
 
     q = vine.Manager()
+    print("Creating Library from functions...")
+    function_lib = q.create_library_from_functions('test-library', func, add_env=False)
+    q.install_library(function_lib)
+
     print("listening on port", q.port)
     factory = vine.Factory("local",manager_host_port="localhost:{}".format(q.port))
     factory.max_workers=1
@@ -43,14 +50,45 @@ def main():
         print("waiting for tasks to complete...")
         end = time.time()
         one = end - start
-    throughput = num_tasks/many
-    chaining = num_tasks/one
+        throughput = num_tasks/many
+        chaining = num_tasks/one
+        #serverless tasks
+        for i in range (num_tasks):
+            t = vine.FunctionCall('test-library', 'func')
+            task_id = q.submit(t)
+
+        print("Waiting for tasks to complete...")
+        start_timer = True
+        while not q.empty():
+            t = q.wait(5)
+            if start_timer:
+                start = time.time()
+                start_timer = False
+        end = time.time()
+        serverless_many = end - start
+
+        start = time.time()
+        for i in range(num_tasks):
+            while not q.empty():
+                result = q.wait(5)
+            t = vine.FunctionCall('test-library', 'func')
+            task_id = q.submit(t)
+        end = time.time()
+        serverless_one = end-start
+    serverless_throughput = num_tasks/serverless_many
+    serverless_chaining = num_tasks/serverless_one
+
+    
     print(f"\nThroughput was {throughput} tasks per second")
     print(f"Chaining was {chaining} tasks per second")
+    print(f"\nServerless Throughput was {serverless_throughput} tasks per second")
+    print(f"Serverless Chaining was {serverless_chaining} tasks per second")
     print("all tasks complete!")
-    assert throughput >= 190
-    assert chaining >= 155
-
+    assert throughput >= 190, "Throughput in python api is less than required 190"
+    assert chaining >= 155, "Throughput for chaining in python api is less than required 155"
+    assert serverless_throughput >= 110, "Throughput using serverless is less than required 190"
+    assert serverless_chaining >= 50, "Throughput for chaining using serverless is less than required 155"
+    
 if __name__ == '__main__':
     main()
 
--- test-vine-task-throughput.sh
+++ test-vine-task-throughput.sh
@@ -7,5 +7,4 @@
 . ./install-github.sh
 
 # Run the test case.
-python test-vine-task-serverless.py
 python test-vine-task-throughput.py
